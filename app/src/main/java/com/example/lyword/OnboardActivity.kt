c71912ed1757e40f4c194c8b4cbb88122a1d1ce4
JIYOUNG PARK <ziy913@naver.com>
2023-05-14 16:53:01 +0900

    [ADD] 시작화면 기능 구현

--- app/src/main/java/com/example/lyword/OnboardActivity.kt
+++ app/src/main/java/com/example/lyword/OnboardActivity.kt
@@ -1,11 +1,66 @@
 package com.example.lyword
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.core.content.ContentProviderCompat.requireContext
+import com.example.lyword.databinding.ActivityMainBinding
+import com.example.lyword.databinding.ActivityOnboardBinding
+import com.example.lyword.databinding.BottomSheetClauseBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class OnboardActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityOnboardBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_onboard)
+        binding = ActivityOnboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.onboardStart.setOnClickListener{
+            openClauseBottomSheet()
+        }
+
     }
+
+    private fun openClauseBottomSheet() {
+        val dialog = BottomSheetDialog(this, R.style.BottomSheetDialogTheme)
+        val dialogBinding =
+            BottomSheetClauseBinding.inflate(LayoutInflater.from(this))
+
+        dialog.setContentView(dialogBinding.root)
+
+        with(dialogBinding) {
+            clauseNextButton.isEnabled = false
+
+            allAgreeCheckBox.setOnClickListener {
+                serviceAgreeCheckBox.isChecked = allAgreeCheckBox.isChecked
+                personalInfoAgreeCheckBox.isChecked = allAgreeCheckBox.isChecked
+                clauseNextButton.isEnabled = allAgreeCheckBox.isChecked
+            }
+
+            serviceAgreeCheckBox.setOnClickListener {
+                allAgreeCheckBox.isChecked = serviceAgreeCheckBox.isChecked && personalInfoAgreeCheckBox.isChecked
+                clauseNextButton.isEnabled = allAgreeCheckBox.isChecked
+            }
+
+            personalInfoAgreeCheckBox.setOnClickListener {
+                allAgreeCheckBox.isChecked = serviceAgreeCheckBox.isChecked && personalInfoAgreeCheckBox.isChecked
+                clauseNextButton.isEnabled = allAgreeCheckBox.isChecked
+            }
+
+            clauseNextButton.setOnClickListener {
+
+                val intent = Intent(applicationContext, MainActivity::class.java)
+                startActivity(intent)
+
+                dialog.dismiss()
+                finish()
+            }
+        }
+        dialog.show()
+    }
+
 }
--- app/src/main/java/com/example/lyword/SplashActivity.kt
+++ app/src/main/java/com/example/lyword/SplashActivity.kt
@@ -1,11 +1,24 @@
 package com.example.lyword
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        moveOnBoard(1)
+    }
+
+    private fun moveOnBoard(sec: Int) {
+        Handler().postDelayed(Runnable {
+            //new Intent(현재 context, 이동할 activity)
+            val intent = Intent(applicationContext, OnboardActivity::class.java)
+            startActivity(intent) //intent 에 명시된 액티비티로 이동
+            finish() //현재 액티비티 종료
+        }, (1000 * sec).toLong()) // sec초 정도 딜레이를 준 후 시작
     }
 }
