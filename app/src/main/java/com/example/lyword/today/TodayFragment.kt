9a00091bcc0d0b5d6845c70b49eca1d11063ebb1
junggon <marco2443@gachon.ac.kr>
2023-05-21 04:31:02 +0900

    Today 페이지에서 "Start To Learn" 버튼 클릭시 Studying 페이지로 이동하는 이벤트 추가

--- app/src/main/java/com/example/lyword/today/TodayFragment.kt
+++ app/src/main/java/com/example/lyword/today/TodayFragment.kt
@@ -5,7 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.lyword.R
 import com.example.lyword.databinding.FragmentTodayBinding
+import com.example.lyword.studying.StudyingFragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.lyword.MainActivity
 
 class TodayFragment : Fragment() {
     lateinit var binding : FragmentTodayBinding
@@ -16,6 +21,17 @@ class TodayFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentTodayBinding.inflate(inflater, container, false)
+
+
+        binding.startToLearn.setOnClickListener {
+            val studyingFragment = StudyingFragment()
+            val fragmentTransaction = childFragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.today_frm, studyingFragment)
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+
+            (requireActivity() as MainActivity).binding.mainBottomNavi.selectedItemId = R.id.studyingFragment
+        }
 
         return binding.root
     }
