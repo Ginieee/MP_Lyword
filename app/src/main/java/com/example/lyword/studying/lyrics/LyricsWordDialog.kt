ea8df8f23168b500577c33be01e381b5fe1ed33a
Ginieee <kaj1226@naver.com>
2023-05-25 14:48:39 +0900

    LywordDatabase 만듦

--- app/src/main/java/com/example/lyword/studying/lyrics/LyricsWordDialog.kt
+++ app/src/main/java/com/example/lyword/studying/lyrics/LyricsWordDialog.kt
@@ -8,7 +8,7 @@ import androidx.room.Room
 import androidx.viewpager2.widget.ViewPager2
 import com.example.lyword.databinding.DialogLyricsWordBinding
 import com.example.lyword.studying.lyrics.word.WordDao
-import com.example.lyword.studying.lyrics.word.WordDatabase
+import com.example.lyword.data.WordDatabase
 import com.example.lyword.studying.lyrics.word.WordEntity
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
