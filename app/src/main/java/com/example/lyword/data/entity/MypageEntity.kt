17985244bf92ac4b314908a2ad26decc72c78704
Ginieee <kaj1226@naver.com>
2023-05-25 15:33:35 +0900

    Mypage Entity 추가

--- app/src/main/java/com/example/lyword/data/entity/MypageEntity.kt
+++ app/src/main/java/com/example/lyword/data/entity/MypageEntity.kt
@@ -1,8 +1,10 @@
 package com.example.lyword.data.entity
 
 import androidx.room.ColumnInfo
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+@Entity
 data class MypageEntity(
     @PrimaryKey(autoGenerate = true)
     var mypageId : Long = 0,
