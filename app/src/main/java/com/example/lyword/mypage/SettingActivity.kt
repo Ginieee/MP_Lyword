c9b9408828c5e4e8526975e348917191ad47a715
JIYOUNG PARK <ziy913@naver.com>
2023-05-26 10:55:52 +0900

    [ADD] kakao Logout 추가

--- app/src/main/java/com/example/lyword/mypage/SettingActivity.kt
+++ app/src/main/java/com/example/lyword/mypage/SettingActivity.kt
@@ -8,6 +8,7 @@ import com.example.lyword.SplashActivity
 import com.example.lyword.databinding.ActivitySettingBinding
 import com.example.lyword.kakao.LoginActivity
 import com.kakao.sdk.common.util.Utility
+import com.kakao.sdk.user.UserApiClient
 
 class SettingActivity : AppCompatActivity() {
 
@@ -25,6 +26,16 @@ class SettingActivity : AppCompatActivity() {
             val intent = Intent(applicationContext, SplashActivity::class.java)
             startActivity(intent)
             finish()
+        }
+
+        binding.logoutBt.setOnClickListener {
+            UserApiClient.instance.logout { error ->
+                if (error != null) {
+                    Log.d("카카오","카카오 로그아웃 실패")
+                }else {
+                    Log.d("카카오","카카오 로그아웃 성공!")
+                }
+            }
         }
 
         binding.icBack.setOnClickListener {
