0febf4a45c14ebd0c8deb745c7b0e0e5b3e4afba
junggon <marco2443@gachon.ac.kr>
2023-05-30 23:51:18 +0900

    LyricsQuizFragment 부분 수정

--- app/src/main/java/com/example/lyword/studying/lyrics/LyricsQuizFragment.kt
+++ app/src/main/java/com/example/lyword/studying/lyrics/LyricsQuizFragment.kt
@@ -47,120 +47,140 @@ LyricsQuizFragment  : Fragment() {
         binding.quiz5Percent.setOnClickListener {
             val intent = Intent(requireContext(), SolvingQuizActivity::class.java)
             intent.putExtra("percent", 5)
+            intent.putExtra("tmpStudyId",studyId)
             startActivity(intent)
 
         }
         binding.quiz10Percent.setOnClickListener {
             val intent = Intent(requireContext(), SolvingQuizActivity::class.java)
             intent.putExtra("percent", 10)
+            intent.putExtra("tmpStudyId",studyId)
             startActivity(intent)
 
         }
         binding.quiz15Percent.setOnClickListener {
             val intent = Intent(requireContext(), SolvingQuizActivity::class.java)
             intent.putExtra("percent", 15)
+            intent.putExtra("tmpStudyId",studyId)
             startActivity(intent)
 
         }
         binding.quiz20Percent.setOnClickListener {
             val intent = Intent(requireContext(), SolvingQuizActivity::class.java)
             intent.putExtra("percent", 20)
+            intent.putExtra("tmpStudyId",studyId)
             startActivity(intent)
 
         }
         binding.quiz25Percent.setOnClickListener {
             val intent = Intent(requireContext(), SolvingQuizActivity::class.java)
             intent.putExtra("percent", 25)
+            intent.putExtra("tmpStudyId",studyId)
             startActivity(intent)
 
         }
         binding.quiz30Percent.setOnClickListener {
             val intent = Intent(requireContext(), SolvingQuizActivity::class.java)
             intent.putExtra("percent", 30)
+            intent.putExtra("tmpStudyId",studyId)
             startActivity(intent)
 
         }
         binding.quiz35Percent.setOnClickListener {
             val intent = Intent(requireContext(), SolvingQuizActivity::class.java)
             intent.putExtra("percent", 35)
+            intent.putExtra("tmpStudyId",studyId)
             startActivity(intent)
 
         }
         binding.quiz40Percent.setOnClickListener {
             val intent = Intent(requireContext(), SolvingQuizActivity::class.java)
             intent.putExtra("percent", 40)
+            intent.putExtra("tmpStudyId",studyId)
             startActivity(intent)
 
         }
         binding.quiz45Percent.setOnClickListener {
             val intent = Intent(requireContext(), SolvingQuizActivity::class.java)
             intent.putExtra("percent", 45)
+            intent.putExtra("tmpStudyId",studyId)
             startActivity(intent)
 
         }
         binding.quiz50Percent.setOnClickListener {
             val intent = Intent(requireContext(), SolvingQuizActivity::class.java)
             intent.putExtra("percent", 50)
+            intent.putExtra("tmpStudyId",studyId)
             startActivity(intent)
 
         }
         binding.quiz55Percent.setOnClickListener {
             val intent = Intent(requireContext(), SolvingQuizActivity::class.java)
             intent.putExtra("percent", 55)
+            intent.putExtra("tmpStudyId",studyId)
             startActivity(intent)
 
         }
         binding.quiz60Percent.setOnClickListener {
             val intent = Intent(requireContext(), SolvingQuizActivity::class.java)
             intent.putExtra("percent", 60)
+            intent.putExtra("tmpStudyId",studyId)
             startActivity(intent)
 
         }
         binding.quiz65Percent.setOnClickListener {
             val intent = Intent(requireContext(), SolvingQuizActivity::class.java)
             intent.putExtra("percent", 65)
+            intent.putExtra("tmpStudyId",studyId)
             startActivity(intent)
 
         }
         binding.quiz70Percent.setOnClickListener {
             val intent = Intent(requireContext(), SolvingQuizActivity::class.java)
             intent.putExtra("percent", 70)
+            intent.putExtra("tmpStudyId",studyId)
             startActivity(intent)
 
         }
         binding.quiz75Percent.setOnClickListener {
             val intent = Intent(requireContext(), SolvingQuizActivity::class.java)
             intent.putExtra("percent", 75)
+            intent.putExtra("tmpStudyId",studyId)
             startActivity(intent)
 
         }
         binding.quiz80Percent.setOnClickListener {
             val intent = Intent(requireContext(), SolvingQuizActivity::class.java)
             intent.putExtra("percent", 80)
+            intent.putExtra("tmpStudyId",studyId)
             startActivity(intent)
 
         }
         binding.quiz85Percent.setOnClickListener {
             val intent = Intent(requireContext(), SolvingQuizActivity::class.java)
             intent.putExtra("percent", 85)
+            intent.putExtra("tmpStudyId",studyId)
             startActivity(intent)
 
         }
         binding.quiz90Percent.setOnClickListener {
             val intent = Intent(requireContext(), SolvingQuizActivity::class.java)
             intent.putExtra("percent", 90)
+            intent.putExtra("tmpStudyId",studyId)
             startActivity(intent)
 
         }
         binding.quiz95Percent.setOnClickListener {
             val intent = Intent(requireContext(), SolvingQuizActivity::class.java)
             intent.putExtra("percent", 95)
+            intent.putExtra("tmpStudyId",studyId)
             startActivity(intent)
 
         }
         binding.quiz100Percent.setOnClickListener {
             val intent = Intent(requireContext(), SolvingQuizActivity::class.java)
             intent.putExtra("percent", 100)
+            intent.putExtra("tmpStudyId",studyId)
             startActivity(intent)
 
         }
@@ -173,6 +193,7 @@ class SolvingQuizActivity : AppCompatActivity() {
     var percent: Int = 0
     var totalCount : Int = 0
     var count : Int = 0
+    var tmpStudyId : Long = 0
 
     lateinit var db : LywordDatabase
     private var solveQuiz : List<WordEntity> = arrayListOf()
@@ -189,12 +210,13 @@ class SolvingQuizActivity : AppCompatActivity() {
         binding = ActivitySolveQuizBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-    /* DB에 접근해서 퀴즈 데이터 불러 오는 부분
+     //DB에 접근해서 퀴즈 데이터 불러 오는 부분
      db = LywordDatabase.getInstance(this)
 
-        getWordFromDB()
+        percent = intent.getIntExtra("percent", 0)
+        tmpStudyId = intent.getLongExtra("tmpStudyId", 0)
 
-        percent = intent.getIntExtra("percentNum", 0)
+        getWordFromDB(tmpStudyId)
 
         totalCount=solveQuiz.size
         count = (totalCount * percent) / 100
@@ -213,12 +235,12 @@ class SolvingQuizActivity : AppCompatActivity() {
 
         binding.lyricsQuiz.text= quizContents[quizIndex].wordOrigin
         binding.romazaQuiz.text= quizContents[quizIndex].wordPronunciation
-        bidning.quizCheck.text= quizContents[quizIndex].wordEnglish // 정답 확인용
+        binding.quizCheck.text= quizContents[quizIndex].wordEnglish // 정답 확인용
 
         binding.quizSelect1.text=quizContents[0].wordEnglish
         binding.quizSelect2.text=quizContents[1].wordEnglish
         binding.quizSelect3.text=quizContents[2].wordEnglish
-        binding.quizSelect4.text=quizContents[3].wordEnglish*/
+        binding.quizSelect4.text=quizContents[3].wordEnglish
 
         binding.quizSelect1.setOnClickListener{
             choice=binding.quizSelect1.text
@@ -257,13 +279,8 @@ class SolvingQuizActivity : AppCompatActivity() {
                     resultCheck=1
                     showCustomDialog()
 
-
-                    /* 정답시 퍼센트를 올리는 부분
-
-
-                    upgradePercentFromDB(?)
-
-                    */
+                    // 정답시 퍼센트를 올리는 부분
+                    upgradePercentFromDB(tmpStudyId)
 
                 }else{ // 오답
                     resultCheck=0
@@ -312,23 +329,19 @@ class SolvingQuizActivity : AppCompatActivity() {
             }
         }
 
-
         dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-
-
-
 
         dialog.show()
     }
 
     // 퀴즈에 사용할 단어를 db에서 가져오는 부분
-    private fun getWordFromDB() {
+    private fun getWordFromDB(tmpId: Long) {
         val thread : Thread = Thread {
 
+            val studyEntity = db.studyDao.getStudyById(tmpId)
+            solveQuiz = studyEntity.wordList!!
 
-            // solveQuiz = db.wordDao.getWordBySentenceIdx(7)
-            solveQuiz = db.wordDao.getWord()
         }
         thread.start()
 
@@ -343,8 +356,7 @@ class SolvingQuizActivity : AppCompatActivity() {
     private fun upgradePercentFromDB(tmpStudyId: Long) {
         val thread : Thread = Thread {
 
-            val studyId = tmpStudyId
-            val studyEntity = db.studyDao.getStudyById(studyId)
+            val studyEntity = db.studyDao.getStudyById(tmpStudyId)
             studyEntity.percent += 5
             db.studyDao.updateStudy(studyEntity)
 
