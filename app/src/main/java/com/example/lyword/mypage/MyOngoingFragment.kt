aac2f52b9accf12c30db2b5bac79860145028a72
JIYOUNG PARK <ziy913@naver.com>
2023-06-02 16:20:32 +0900

    [ADD] mypage에서 study 항목 추가

--- app/src/main/java/com/example/lyword/mypage/MyOngoingFragment.kt
+++ app/src/main/java/com/example/lyword/mypage/MyOngoingFragment.kt
@@ -1,18 +1,26 @@
 package com.example.lyword.mypage
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.lyword.data.LywordDatabase
 import com.example.lyword.data.entity.StudyEntity
 import com.example.lyword.databinding.FragmentMyOngoingBinding
 import com.example.lyword.studying.StudyingSongRVAdapter
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class MyOngoingFragment : Fragment() {
     lateinit var binding: FragmentMyOngoingBinding
+    lateinit var db: LywordDatabase
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,17 +29,26 @@ class MyOngoingFragment : Fragment() {
     ): View? {
         binding = FragmentMyOngoingBinding.inflate(inflater, container, false)
 
-        initRV()
+        db = LywordDatabase.getInstance(requireContext())
+
+        GlobalScope.launch(Dispatchers.Main) {
+            initRV()
+        }
 
         return binding.root
     }
 
-    private fun initRV(){
-
-        val studys = ArrayList<StudyEntity>()
-
-        val rvAdapter = StudyingSongRVAdapter(studys, requireContext())
-        binding.studyRecordRv.adapter = rvAdapter
-        binding.studyRecordRv.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+    @OptIn(DelicateCoroutinesApi::class)
+    private suspend fun initRV(){
+        withContext(Dispatchers.IO) {
+            Log.d("Studying", db.studyDao.getStudyList().toString())
+            val songs = db.studyDao.getStudyList() as ArrayList<StudyEntity>
+            withContext(Dispatchers.Main) {
+                val rvAdapter = StudyingSongRVAdapter(songs, requireContext())
+                binding.studyRecordRv.adapter = rvAdapter
+                binding.studyRecordRv.layoutManager =
+                    LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            }
+        }
     }
 }
